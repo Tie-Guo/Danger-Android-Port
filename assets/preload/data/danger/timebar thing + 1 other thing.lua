69150d5fbcd7b0947bcfe5f8437bc68a91489a6c
Tie-Guo <117576846+Tie-Guo@users.noreply.github.com>
2023-01-09 17:23:02 +0800

    add chart

---
+++ assets/preload/data/danger/timebar thing + 1 other thing.lua
@@ -0,0 +1,7 @@
+function onCreatePost()
+setProperty('timeBar.color', getColorFromHex('3f921b'))
+
+setObjectOrder('gf', 13)
+setObjectOrder('Blacks legs', 14)
+setProperty('scoreTxt.color', getColorFromHex('3A1B4F'))
+end
